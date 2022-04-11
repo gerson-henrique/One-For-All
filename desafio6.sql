531add096c97ebe098ab458f30c9e2e17d49eac1
Gero <gersonhoa@gmail.com>
2022-04-11 19:29:30 -0300

    desafio6.sql

--- desafio6.sql
+++ desafio6.sql
@@ -2,6 +2,6 @@ SELECT ROUND(MIN(plans.price),2) as "faturamento_minimo",
    ROUND(MAX(plans.price),2) as "faturamento_maximo",
   ROUND(AVG(plans.price),2)  as "faturamento_medio",
   ROUND(SUM(plans.price),2) as "faturamento_total"
-FROM plans
+FROM plans as plans
 JOIN assingments as assingments
-ON assingments.plan_id = assingments.plan_id;
+ON assingments.plan_id = plans.id;
